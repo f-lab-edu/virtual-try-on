930d057ff32a4d00757842d9fec0a5f6815aab4f
seuno2 <levn94@gmail.com>
2024-03-05 03:08:58 +0900

    fix configuration

--- main.py
+++ main.py
@@ -10,39 +10,39 @@ import uvicorn
 from PIL import Image
 
 from model import VTryOnModel
-from config.config import Configuration
+from config.config import opt
 import preprocess
 
 
-config = Configuration()
-
+# config = Configuration()
 logging.basicConfig(filename='error.log', level=logging.DEBUG)
 
-app = FastAPI()
 
+app = FastAPI()
 
 @app.post("/upload/")
 async def upload_image(cloth: UploadFile = File(...), person: UploadFile = File(...)):
     try:
         # Model Initialization
-        vton = VTryOnModel(config.device)
+        vton = VTryOnModel(opt.device)
 
-        # Preprocessing
-        cloth_path = config.cloth_path + f"{uuid.uuid4()}.jpg"
+        # Uploading Images
+        cloth_path = opt.cloth_path + f"{uuid.uuid4()}.jpg"
         with open(cloth_path, "wb") as img_file:
             img_file.write(await cloth.read())
 
-        if not config.edge_exist:
+        if not opt.edge_exist:
             edge_name = f"{uuid.uuid4()}.jpg"
-            edge_path = config.edge_path + edge_name
+            edge_path = opt.edge_path + edge_name
             preprocess.generate_edge(edge_exist=False, img_path=cloth_path, output_name=edge_name)
         else:
             edge_path = "dataset/service_edge/ada6385c-a2e7-44a1-a48d-6b0b64d47963.jpg"
 
-        person_path = config.person_path + f"{uuid.uuid4()}.jpg"
+        person_path = opt.person_path + f"{uuid.uuid4()}.jpg"
         with open(person_path, "wb") as img_file:
             img_file.write(await person.read())
 
+        # Preprocessing
         for img_path in [cloth_path, edge_path, person_path]:
             im = Image.open(img_path)
             if im.size != (192, 256):
