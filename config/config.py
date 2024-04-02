745f8c2e705fe9f40ac55afc187408a5a1e514b7
seuno2 <levn94@gmail.com>
2024-04-03 00:27:33 +0900

    background task 비동기 처리

--- config/config.py
+++ config/config.py
@@ -14,14 +14,9 @@ class Configuration(object):
     def load_config(self, config_file="config/config.yaml"):
         with open(config_file, 'r') as f:
             cfg = yaml.safe_load(f)
-
-
             self.device = "gpu" if cfg["device"] else "cpu"
-            
             self.root = cfg["data"]["root"]
-
             self.edge_exist = cfg["data"]["edge_exist"]
-            
             self.cloth_path = cfg["data"]["cloth_path"]
             self.edge_path = cfg["data"]["edge_path"]
             self.person_path = cfg["data"]["person_path"]
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import logging
 import traceback
 import uuid
 
-# from async_generator import asynccontextmanager
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks, HTTPException
 from fastapi.responses import FileResponse
 import uvicorn
@@ -14,31 +13,25 @@ from model import VTryOnModel
 from config.config import opt
 import preprocessor
 
-logging.basicConfig(filename='error.log', level=logging.DEBUG)
 
+logging.basicConfig(filename='error.log', level=logging.DEBUG)
 app = FastAPI()
 vton = VTryOnModel(opt.device)
 
-async def preprocessing(cloth_path, edge_path, person_path):
-    for path in [cloth_path, edge_path, person_path]:
-        im = Image.open(path)
-        if im.size != (192, 256):
-            preprocessor.resize(path)
+async def generate_img(cloth_path, person_path):
+    edge_name = f"{uuid.uuid4()}.jpg"
+    edge_path = opt.edge_path + edge_name
+    preprocessor.generate_edge(edge_exist=False, device='cpu', img_path=cloth_path, output_path=opt.edge_path,output_name=edge_name)
+
+    preprocessor.resize(cloth_path)
+    preprocessor.resize(person_path) 
 
     c_tensor, e_tensor, p_tensor = preprocessor.img_to_tensor(cloth_path, edge_path, person_path)
-
-    return c_tensor, e_tensor, p_tensor
-
-
-async def inference(c_tensor, e_tensor, p_tensor):
     vton.infer(c_tensor, e_tensor, p_tensor)
-    result_image_path = opt.root + "results/output.jpg"
-    
-    return FileResponse(result_image_path, media_type="image/jpeg")
 
 
 @app.post("/upload-images")
-async def upload_image(cloth: UploadFile = File(...), person: UploadFile = File(...) ):
+async def upload_image(background_tasks: BackgroundTasks, cloth: UploadFile = File(...), person: UploadFile = File(...)):
     try:
         cloth_path = opt.cloth_path + f"{uuid.uuid4()}.jpg"
         person_path = opt.person_path + f"{uuid.uuid4()}.jpg"
@@ -48,19 +41,8 @@ async def upload_image(cloth: UploadFile = File(...), person: UploadFile = File(
         with open(person_path, "wb+") as img_file:
             img_file.write(person.file.read())
 
-        if not opt.edge_exist:
-            edge_name = f"{uuid.uuid4()}.jpg"
-            edge_path = opt.edge_path + edge_name
-            preprocessor.generate_edge(edge_exist=False, device='cpu', img_path=cloth_path, output_path=opt.edge_path,output_name=edge_name)
-        else:
-            edge_path = opt.root + "dataset/service_edge/ada6385c-a2e7-44a1-a48d-6b0b64d47963.jpg"
-
-        c_tensor, e_tensor, p_tensor = await preprocessing(cloth_path, edge_path, person_path)
-        
-        await inference(c_tensor, e_tensor, p_tensor)
-
-        result_image_path = opt.root + "results/output.jpg"
-        return FileResponse(result_image_path, media_type="image/jpeg") 
+        background_tasks.add_task(generate_img, cloth_path, person_path)
+        return {"message": "Generating image in the background"}
 
     except Exception as e:
         logging.error(traceback.format_exc())
